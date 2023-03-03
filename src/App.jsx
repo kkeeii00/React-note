9c89f65f3db72237016f2af8d5879abd9626f59b
kkeeii00 <gavicsk@gmail.com>
2023-03-03 19:29:48 +0900

    reactnote

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import uuid from "react-uuid";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState();
+
+  const onAddNote = () => {
+    // console.log('新しくノートが出来ました')
+    const newNote = {
+      id: uuid(),
+      title: "title",
+      content: "content",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    //修正されたノートの配列を返す
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note
+      }
+    });
+
+    setNotes(updatedNotesArray);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar
+        onAddNote={onAddNote}
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  );
+}
+
+export default App;
